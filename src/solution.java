084b475fd623d0ba1e789e226df4b9b8cc3ff622
khaniz <parkizkhan@gmail.com>
2022-07-07 22:05:35 +0900

    BAEKJOON No.1001 to No.25083
    COMPLETE STEP1

--- src/solution.java
+++ src/solution.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class solution {
 
     public void dog(){
@@ -38,6 +40,123 @@ public class solution {
         int b = getInt.nextInt();
 
         System.out.println(a-b);
+    }
+
+    public void no10998(){
+
+        Scanner getInt = new Scanner(System.in);
+
+        int a = getInt.nextInt();
+        int b = getInt.nextInt();
+
+        getInt.close();
+
+        System.out.println(a*b);
+
+    }
+
+    public void no1008(){
+        Scanner getInt = new Scanner(System.in);
+
+        int a = getInt.nextInt();
+        int b = getInt.nextInt();
+
+        getInt.close();
+
+        System.out.println((double)a/b);
+
+    }
+
+    public void no10869(){
+
+        Scanner getInt = new Scanner(System.in);
+
+        int a = getInt.nextInt();
+        int b = getInt.nextInt();
+
+        getInt.close();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+
+    public void no10926(){
+
+        Scanner getStringLine = new Scanner(System.in);
+
+        String id = getStringLine.nextLine();
+        getStringLine.close();
+
+        System.out.println(id+ "??!");
+    }
+
+    public void no18108(){
+
+        Scanner getInt = new Scanner(System.in);
+
+        int time = getInt.nextInt();
+
+        while(true){
+            if(time <= 1000 || time >= 3000){
+                System.out.println("1000~3000 사이의 연도를 입력해주세요");
+                time = getInt.nextInt();
+            }else {
+                break;
+            }
+        }
+        getInt.close();
+
+        int ac = time-543;
+        System.out.println(ac);
+
+    }
+
+    public void no10430(){
+
+        Scanner getInt = new Scanner(System.in);
+
+        int a = getInt.nextInt();
+        int b = getInt.nextInt();
+        int c = getInt.nextInt();
+
+        getInt.close();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c) + (b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+
+    public void no2588(){
+        Scanner getInt = new Scanner(System.in);
+
+        int a = getInt.nextInt();
+        int b = getInt.nextInt();
+        getInt.close();
+
+        String strB = Integer.toString(b);
+        String first = strB.substring(2,3);
+        String second = strB.substring(1,2);
+        String third = strB.substring(0,1);
+
+        System.out.println(a*parseInt(first));
+        System.out.println(a*parseInt(second));
+        System.out.println(a*parseInt(third));
+        System.out.println(a*b);
+
+    }
+
+    public void no25083(){
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
     }
 
 }
