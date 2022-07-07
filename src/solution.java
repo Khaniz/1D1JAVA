b46f4f9b917d4f61057f098dd00cc8c44f4b64eb
khaniz <parkizkhan@gmail.com>
2022-07-07 20:57:40 +0900

    BAEKJOON No.1001 Input 3-2 Print Result

--- src/solution.java
+++ src/solution.java
@@ -12,13 +12,32 @@ public class solution {
 
     }
 
-    public void additionOneTwo(){
+    public void subs2(){
 
         Scanner getInt = new Scanner(System.in);
         int a = getInt.nextInt();
+        while(0 >= a){
+            System.out.println("첫 입력은 0보다 커야합니다");
+            a = getInt.nextInt();
+        }
+
+        int b = getInt.nextInt();
+        while(b >= 10){
+            System.out.println("마지막 입력은 10보다 작아야합니다");
+            b = getInt.nextInt();
+        }
+
+        System.out.println(a-b);
+    }
+
+    public void substractionThreeTwo(){
+
+        Scanner getInt = new Scanner(System.in);
+        int a = getInt.nextInt();
+
         int b = getInt.nextInt();
 
-        System.out.println(a+b);
+        System.out.println(a-b);
     }
 
 }
