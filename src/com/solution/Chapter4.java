94d18b1754c828e7cebf6264cc07de1208586a36
khaniz <gjf.kee@gmail.com>
2022-07-14 20:27:27 +0900

    BAEKJOON Chapter4 no2562
    최댓값과 인덱스

--- src/com/solution/Chapter4.java
+++ src/com/solution/Chapter4.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Chapter4 {
 
@@ -23,5 +24,20 @@ public class Chapter4 {
 
         strings.stream().min(Comparator.comparing(a -> a)).ifPresent(s-> System.out.print(s+" "));
         strings.stream().max(Comparator.comparing( a -> a)).ifPresent(s-> System.out.print(s));
+    }
+
+    public void no2562() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i<9; i++){
+            int n = Integer.parseInt(br.readLine());
+            numbers.add(i, n);
+        }
+
+        AtomicInteger value = new AtomicInteger();
+        numbers.stream().max(Comparator.comparing( a -> a)).ifPresent(s-> value.set(s));
+
+        System.out.println(value);
+        System.out.println(numbers.indexOf(Integer.parseInt(String.valueOf(value)))+1);
     }
 }
