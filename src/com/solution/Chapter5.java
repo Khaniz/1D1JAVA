2ff681b6c36cedcabbb854a73aefbc7c9011ad56
khaniz <gjf.kee@gmail.com>
2022-07-18 21:56:33 +0900

    BAEKJOON Chapter5 no4673
    셀프넘버

--- src/com/solution/Chapter5.java
+++ src/com/solution/Chapter5.java
@@ -1,5 +1,8 @@
 package com.solution;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Chapter5 {
 
     public void no15596(){
@@ -9,6 +12,29 @@ public class Chapter5 {
 
     }
 
+    public void no4673(){
+        List<Integer> selfNums = new ArrayList<>();
+
+        for(int i=0; i<10000; i++){
+            int selfNum = new selfNumber().d(i);
+            selfNums.add(selfNum);
+        }
+        int compare = 1;
+        for(int i= 0; i<selfNums.size(); i++){
+
+            boolean hasSelfNo = false;
+            for(int self : selfNums){
+                if(self == compare){
+                    hasSelfNo = true;
+                }
+            }
+            if(!hasSelfNo){
+                System.out.println(compare);
+            }
+            compare++;
+        }
+    }
+
     public long Test(int[] a){
         long sum = 0;
         for(int i : a){
@@ -16,4 +42,27 @@ public class Chapter5 {
         }
         return sum;
     }
+}
+
+class selfNumber {
+
+    public int d(int n){
+        int d = n;
+        int temp = 0;
+        int result = n;
+
+        while(true){
+            temp = d%10; // 1의 자리수
+            result += temp;
+
+            if(d<10){
+                break;
+            }
+            d = d/10;
+
+        }
+
+        return result;
+    }
+
 }
