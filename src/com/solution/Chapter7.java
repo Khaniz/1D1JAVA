d0390fdb90607a71c6aa6aeef70a4ceaebd7c0d1
khaniz <gjf.kee@gmail.com>
2022-08-02 21:48:05 +0900

    BAEKJOON Chapter7 no2869
    시간초과

--- src/com/solution/Chapter7.java
+++ src/com/solution/Chapter7.java
@@ -90,8 +90,26 @@ public class Chapter7 {
             }
         }
     }
-    public void no2869(){
+    public void no2869() throws IOException {
         //달팽이는 올라가고 싶다
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        br.close();
+
+        int tempDaily = 0;
+        for(int i = 1; i<v; i++){
+            if(i*a - (i-1)*b >= v){
+                tempDaily = i;
+                break;
+            }
+        }
+
+        System.out.println(tempDaily);
     }
     public void no10250(){
         //ACM 호텔
