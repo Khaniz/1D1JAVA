60265bf65a5cf2f6cfd2efd02ef3e53eb93ed84a
Khan <gjf.kee@gmail.com>
2024-09-03 22:45:34 +0900

    test

---
+++ src/com/solution/Chapter10.java
@@ -0,0 +1,8 @@
+package com.solution;
+
+public class Chapter10 {
+    public static void main(String[] args) {
+        System.out.println("Hi, there");
+        System.out.println("restart from here");
+    }
+}
